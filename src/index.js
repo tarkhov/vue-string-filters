b52528d3d57692fed428dcdabb3e0cbbc5e39dbd
tarkhov <agtuks@gmail.com>
2026-01-31 20:48:29 +0200

    Vue3 code update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { createApp } from 'vue'
 import camel from './filters/camel'
 import capitalize from './filters/capitalize'
 import kebab from './filters/kebab'
@@ -11,19 +12,22 @@ import upper from './filters/upper'
 import StringFiltersMixin from './mixins/StringFiltersMixin'
 
 const VueStringFilters = {
-  install (Vue) {
-    Vue.directive('camel', camel)
-    Vue.directive('capitalize', capitalize)
-    Vue.directive('kebab', kebab)
-    Vue.directive('lower', lower)
-    Vue.directive('pad', pad)
-    Vue.directive('repeat', repeat)
-    Vue.directive('replace', replace)
-    Vue.directive('snake', snake)
-    Vue.directive('truncate', truncate)
-    Vue.directive('upper', upper)
+  install(app) {
+    app.directive('camel', camel)
+    app.directive('capitalize', capitalize)
+    app.directive('kebab', kebab)
+    app.directive('lower', lower)
+    app.directive('pad', pad)
+    app.directive('repeat', repeat)
+    app.directive('replace', replace)
+    app.directive('snake', snake)
+    app.directive('truncate', truncate)
+    app.directive('upper', upper)
   }
 }
+
+const app = createApp({})
+app.use(VueStringFilters)
 
 export {
   camel,
@@ -40,7 +44,3 @@ export {
 }
 
 export default VueStringFilters
-
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(VueStringFilters)
-}
